1d2a2fc3207bc567c226afe3676f4de28d735114
PuffyKid <Matt.Puffy@gmail.com>
2015-08-28 13:37:19 -0500

    Create UART.h

---
+++ UART.h
@@ -0,0 +1,18 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+class UART
+{
+	public:
+		UART();
+		void initSynchronous();
+		void initAsynchronous();
+		void setBaudRate()
+			
+	protected:
+		uint8_t 
+		
+	
+	private:
+	
+};
